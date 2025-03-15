fc48058c846e8584995df8ee7668960ca0a244dc
Brunno Santana <brunnosantana98@gmail.com>
2025-03-15 14:58:05 -0300

    alterando o tipo dos ids nas Migrations

--- infrastructure/src/main/resources/db/migration/V1__initial.sql
+++ infrastructure/src/main/resources/db/migration/V1__initial.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE category (
-    id VARCHAR(36) PRIMARY KEY,
+    id CHAR(32) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(4000),
     active BOOLEAN NOT NULL DEFAULT TRUE,
--- infrastructure/src/main/resources/db/migration/V3__create_cast_member_aggregate.sql
+++ infrastructure/src/main/resources/db/migration/V3__create_cast_member_aggregate.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE cast_members (
-    id VARCHAR(36) PRIMARY KEY,
+    id CHAR(32) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     type VARCHAR(255) NOT NULL,
     created_at DATETIME(6) NOT NULL,
