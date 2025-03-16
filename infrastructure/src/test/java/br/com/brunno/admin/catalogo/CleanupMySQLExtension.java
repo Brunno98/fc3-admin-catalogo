f74877e7e1d77fd7905a8fecdf69c7376d98c13a
Brunno Santana <brunnosantana98@gmail.com>
2025-03-16 19:54:59 -0300

    Retornando para solução anterior para que se possa definir a ordem de clean

--- infrastructure/src/test/java/br/com/brunno/admin/catalogo/CleanupMySQLExtension.java
+++ infrastructure/src/test/java/br/com/brunno/admin/catalogo/CleanupMySQLExtension.java
@@ -1,11 +1,18 @@
 package br.com.brunno.admin.catalogo;
 
+import br.com.brunno.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
+import br.com.brunno.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import br.com.brunno.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import br.com.brunno.admin.catalogo.infrastructure.video.persistence.VideoRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class CleanupMySQLExtension implements BeforeEachCallback {
 
@@ -13,9 +20,12 @@ public class CleanupMySQLExtension implements BeforeEachCallback {
     public void beforeEach(ExtensionContext extensionContext) throws Exception {
         final var applicationContext = SpringExtension.getApplicationContext(extensionContext);
 
-        final var crudRepositoryMap = applicationContext.getBeansOfType(CrudRepository.class);
-
-        cleanUp(crudRepositoryMap.values());
+        cleanUp(List.of(
+                applicationContext.getBean(VideoRepository.class),
+                applicationContext.getBean(GenreRepository.class),
+                applicationContext.getBean(CategoryRepository.class),
+                applicationContext.getBean(CastMemberRepository.class)
+        ));
     }
 
     private void cleanUp(Collection<CrudRepository> repositories) {
