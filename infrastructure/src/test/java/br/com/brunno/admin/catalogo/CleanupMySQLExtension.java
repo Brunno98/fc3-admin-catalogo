e3884be883c6d464aa603826b9dacc181f90135d
Brunno Santana <brunnosantana98@gmail.com>
2025-03-16 18:35:07 -0300

    Carregando dinamicamente os repositories no CleanupMySQLExtension

--- infrastructure/src/test/java/br/com/brunno/admin/catalogo/CleanupMySQLExtension.java
+++ infrastructure/src/test/java/br/com/brunno/admin/catalogo/CleanupMySQLExtension.java
@@ -1,15 +1,11 @@
 package br.com.brunno.admin.catalogo;
 
-import br.com.brunno.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
-import br.com.brunno.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
-import br.com.brunno.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collection;
-import java.util.List;
 
 public class CleanupMySQLExtension implements BeforeEachCallback {
 
@@ -17,14 +13,12 @@ public class CleanupMySQLExtension implements BeforeEachCallback {
     public void beforeEach(ExtensionContext extensionContext) throws Exception {
         final var applicationContext = SpringExtension.getApplicationContext(extensionContext);
 
-        cleanUp(List.of(
-                applicationContext.getBean(GenreRepository.class),
-                applicationContext.getBean(CategoryRepository.class),
-                applicationContext.getBean(CastMemberRepository.class)
-        ));
+        final var crudRepositoryMap = applicationContext.getBeansOfType(CrudRepository.class);
+
+        cleanUp(crudRepositoryMap.values());
     }
 
-    private void cleanUp(Collection<CrudRepository<?, ?>> repositories) {
+    private void cleanUp(Collection<CrudRepository> repositories) {
         repositories.forEach(CrudRepository::deleteAll);
     }
 
