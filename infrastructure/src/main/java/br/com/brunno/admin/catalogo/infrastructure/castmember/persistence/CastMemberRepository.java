8c1ad400d28f0f245f75f301bc20a813091ea227
Brunno Santana <brunnosantana98@gmail.com>
2025-03-17 23:05:46 -0300

    implementacao e testes de existsByIds de CastMember

--- infrastructure/src/main/java/br/com/brunno/admin/catalogo/infrastructure/castmember/persistence/CastMemberRepository.java
+++ infrastructure/src/main/java/br/com/brunno/admin/catalogo/infrastructure/castmember/persistence/CastMemberRepository.java
@@ -4,9 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface CastMemberRepository extends JpaRepository<CastMemberJpaEntity, String> {
 
     Page<CastMemberJpaEntity> findAll(Specification<CastMemberJpaEntity> whereClause, Pageable page);
 
+    @Query("SELECT cm.id FROM CastMember cm WHERE cm.id IN :ids")
+    Collection<String> existsByIds(@Param("ids") List<String> castMemberIds);
 }
