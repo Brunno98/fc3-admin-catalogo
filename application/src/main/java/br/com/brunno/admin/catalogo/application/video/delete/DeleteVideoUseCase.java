f214a00d3cb75f4c1fa37fc189e3967dee8411fa
Brunno Santana <brunnosantana98@gmail.com>
2025-03-20 22:58:14 -0300

    Limpando recursos de media apos a delecao de um video

--- application/src/main/java/br/com/brunno/admin/catalogo/application/video/delete/DeleteVideoUseCase.java
+++ application/src/main/java/br/com/brunno/admin/catalogo/application/video/delete/DeleteVideoUseCase.java
@@ -1,19 +1,25 @@
 package br.com.brunno.admin.catalogo.application.video.delete;
 
 import br.com.brunno.admin.catalogo.application.UnitUseCase;
+import br.com.brunno.admin.catalogo.domain.video.MediaResourceGateway;
 import br.com.brunno.admin.catalogo.domain.video.VideoGateway;
 import br.com.brunno.admin.catalogo.domain.video.VideoID;
+
+import java.util.Objects;
 
 public class DeleteVideoUseCase extends UnitUseCase<VideoID> {
 
     private final VideoGateway videoGateway;
+    private final MediaResourceGateway mediaResourceGateway;
 
-    public DeleteVideoUseCase(VideoGateway videoGateway) {
-        this.videoGateway = videoGateway;
+    public DeleteVideoUseCase(VideoGateway videoGateway, MediaResourceGateway mediaResourceGateway) {
+        this.videoGateway = Objects.requireNonNull(videoGateway);
+        this.mediaResourceGateway = Objects.requireNonNull(mediaResourceGateway);
     }
 
     @Override
     public void execute(VideoID anId) {
         this.videoGateway.deleteById(anId);
+        this.mediaResourceGateway.clearResources(anId);
     }
 }
