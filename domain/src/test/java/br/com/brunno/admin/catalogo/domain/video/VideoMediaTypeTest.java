cf7babebfdda1f5732e0bf8d04105336aec09737
Brunno Santana <brunnosantana98@gmail.com>
2025-03-23 20:25:28 -0300

    teste de conversao de valor para VideoMediaType

--- domain/src/test/java/br/com/brunno/admin/catalogo/domain/video/VideoMediaTypeTest.java
+++ domain/src/test/java/br/com/brunno/admin/catalogo/domain/video/VideoMediaTypeTest.java
@@ -1,16 +1,38 @@
 package br.com.brunno.admin.catalogo.domain.video;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class VideoMediaTypeTest {
 
-    @Test
-    void valueOf() {
-        // given
-        // when
-        // then
-        fail("TODO: implementar teste de conversão do enum VideoMediaType");
+    @ParameterizedTest
+    @EnumSource(VideoMediaType.class)
+    void givenAValidValue_whenGetVideoMediaTypeFromStringValue_shouldReturnIt(VideoMediaType expectedType) {
+        final var actualType = VideoMediaType.of(expectedType.name());
+        assertTrue(actualType.isPresent());
+        assertEquals(expectedType, actualType.get());
+    }
+
+    @ParameterizedTest
+    @EnumSource(VideoMediaType.class)
+    void givenAValidLowerCaseValue_whenGetVideoMediaTypeFromStringValue_shouldReturnIt(VideoMediaType expectedType) {
+        final var actualType = VideoMediaType.of(expectedType.name().toLowerCase());
+        assertTrue(actualType.isPresent());
+        assertEquals(expectedType, actualType.get());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"invalid"})
+    void givenAnInvalidValue_whenGetVideoMediaTypeFromStringValue_shouldReturnEmpty(String invalidValue) {
+        assertTrue(VideoMediaType.of(invalidValue).isEmpty());
     }
 }
