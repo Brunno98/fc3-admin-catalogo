9b6cae2c84cecc22a8ad20f50213e276eb371a50
Brunno Santana <brunnosantana98@gmail.com>
2025-03-27 00:22:17 -0300

    adicionando observabilidade ao projeto

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -5,11 +5,22 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Criar as pastas com permissões
 mkdir -m 777 "$SCRIPT_DIR"/.docker
 mkdir -m 777 "$SCRIPT_DIR"/.docker/keycloak
+mkdir -m 777 "$SCRIPT_DIR"/.docker/es01
+mkdir -m 777 "$SCRIPT_DIR"/.docker/filebeat
+sudo chown root "$SCRIPT_DIR"/app/filebeat/filebeat.docker.yml
 
+# Cria as networks docker
 docker network create adm_videos_services
+docker network create elastic
 
+# Sobe os container dos composes
 if ! docker compose  -f "$SCRIPT_DIR"/services/docker-compose.yml up -d; then
-  echo "Falha ao inicializar os containers"
+  echo "Falha ao inicializar os containers services"
+  exit 1
+fi
+
+if ! docker compose  -f "$SCRIPT_DIR"/elk/docker-compose.yml up -d; then
+  echo "Falha ao inicializar os containers elk"
   exit 1
 fi
 
